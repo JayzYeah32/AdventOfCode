6bab696c3b3526f6d7386d9af11e1beb95f9c24c
JayzYeah32 <48171128+JayzYeah32@users.noreply.github.com>
2020-12-07 20:00:13 +1100

    Update Day 6.cs

--- 2020/Day 6.cs
+++ 2020/Day 6.cs
@@ -4,11 +4,4 @@ string report = File.ReadAllText("Numbers.txt").Replace("\r", "");
 Console.WriteLine(Regex.Split(report, "\n\n").Select(x => x.Replace("\n", "").Distinct().Count()).Sum(x => x));
 
  // part 2
-int overlap(string[] s)
-{
-	IEnumerable<char> first = s.First();
-	foreach (string x in s.Skip(1))
-		first = first.Intersect(x.ToCharArray());
-	return first.Count();
-}
-Console.WriteLine(Regex.Split(report, "\n\n").Select(x => overlap(x.Split('\n'))).Sum(x => x));
+Console.WriteLine(Regex.Split(report, "\n\n").Select(x => x.Split('\n').Cast<IEnumerable<char>>().Aggregate((p, n) => p.Intersect(n)).Count()).Sum(x => x));
